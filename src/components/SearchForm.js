2ce314e837f3e2cce3926c2ebf6f18a19e6229c3
deborahlim <deborahlimhy@gmail.com>
2022-04-05 21:11:59 +0800

    fix search term submit function

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,30 +1,36 @@
-import Form from "react-bootstrap/Form"
-import FormControl from "react-bootstrap/FormControl";
-import Button from "react-bootstrap/Button"
-import {useState} from "react";
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
-import {Link} from "react-router-dom"
+import Form from "react-bootstrap/Form";
+import FormControl from "react-bootstrap/FormControl";
+import Button from "react-bootstrap/Button";
 
 const SearchForm = () => {
-
   const [term, setTerm] = useState("");
+  let history = useHistory();
   const submissionFormHandler = (event) => {
-        event.preventDefault();
-        setTerm("");
-        console.log(term)
-    }
-    return (
-        <Form className="d-flex" onSubmit={submissionFormHandler}>
-        <FormControl
-          type="search"
-          placeholder="Type anything here..."
-          value={term}
-          onChange={(event) => setTerm(event.target.value)}
-          className="me-2"
-          aria-label="Search"
-        />
-        <Button variant="outline-success" as={Link} to={`/news/search/${term}`}>Search</Button>
-      </Form>
-    )
-}
+    event.preventDefault();
+    history.push(`/news/search/${term}`);
+    setTerm("");
+  };
+  return (
+    <Form className="d-flex" onSubmit={submissionFormHandler}>
+      <FormControl
+        type="search"
+        placeholder="Type anything here..."
+        value={term}
+        onChange={(event) => setTerm(event.target.value)}
+        onKeyPress={(e) => e.code === 13 && submissionFormHandler}
+        className="me-2"
+        aria-label="Search"
+      />
+      <Button
+        variant="outline-success"
+        type="submit"
+      >
+        Search
+      </Button>
+    </Form>
+  );
+};
 export default SearchForm;
