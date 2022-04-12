8f84b50c4ccbedf8d88796e55e65c497547f2426
deborahlim <deborahlimhy@gmail.com>
2022-04-12 22:38:41 +0800

    format date

--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -1,15 +1,19 @@
-
 import { Card } from "react-bootstrap";
 import "./NewsCard.css";
 import Col from "react-bootstrap/Col";
 const NewsCard = (props) => {
-  const { image, url, description, title } = props.article;
+  const { image, url, description, title, publishedAt, source } = props.article;
 
   const truncateDescription = (description) => {
     let reDot = /[.]/g;
     let indexOfDot = description.search(reDot);
     return description.slice(0, indexOfDot + 1);
   };
+
+  const formatDate = (str) => {
+    const dateTimeStr = new Date(str).toString();
+    return dateTimeStr;
+  }
 
   return (
     <Col className="g-4">
@@ -21,15 +25,16 @@ const NewsCard = (props) => {
         style={{ cursor: "pointer" }}
       >
         <Card className="card">
-          <Card.Img
-            variant="top"
-            src={image}
-            className="card-img"
-          />
+          <Card.Img variant="top" src={image} className="card-img" />
           <Card.Body>
             <Card.Title>{title}</Card.Title>
             <Card.Text>{truncateDescription(description)}</Card.Text>
           </Card.Body>
+          <Card.Footer className="text-muted">
+            Published {formatDate(publishedAt)}
+            <br/>
+            Source: {source.name} 
+          </Card.Footer>
         </Card>
       </Card.Link>
     </Col>
