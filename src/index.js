214ea5a274afe057ce167ae707b35d5c7bf037ad
deborahlim <deborahlimhy@gmail.com>
2022-04-17 23:58:53 +0800

    connect store to the app

--- src/index.js
+++ src/index.js
@@ -3,14 +3,18 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import store from "./redux/store"
+import { Provider } from "react-redux";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
     {/* import BrowserRouter component, wrap root app component with the BrowserRouter component; To activate react router and to make route components and other react router features work */}
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
