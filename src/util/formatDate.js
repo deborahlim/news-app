ae708eb447714be282c26f72042dd10cf07af6b3
deborahlim <deborahlimhy@gmail.com>
2022-06-16 14:40:01 +0800

    add year

--- src/util/formatDate.js
+++ src/util/formatDate.js
@@ -3,11 +3,14 @@ export function msToTime(ms) {
   let minutes = (ms / (1000 * 60)).toFixed();
   let hours = (ms / (1000 * 60 * 60)).toFixed();
   let days = (ms / (1000 * 60 * 60 * 24)).toFixed();
+  let years = (ms / (1000 * 60 * 60 * 24 * 365)).toFixed();
   if (seconds < 60) return seconds + " Sec";
   else if (minutes < 60) return minutes + " Min";
   else if (hours < 24 && hours > 1) return hours + " Hrs";
-  else if (hours >= 1) return hours + " Hr";
-  else return days > 1 ? days + " Days" : days + " Day";
+  else if (hours < 24 && hours >= 1) return hours + " Hr";
+  else if (days < 365 && days > 1) return days + " Days";
+  else if (days < 365 && days <= 1) return days + " Day";
+  else return years > 1 ? years + " Years" : years + " Year";
 }
 export const timeElaspedSinceCurr = (str) => {
   const timeElasped = msToTime(Date.now() - new Date(str).getTime());
