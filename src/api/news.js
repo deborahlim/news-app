e0aee68e4d0fc46e443a2cf225c256cb15279659
deborahlim <deborahlimhy@gmail.com>
2022-06-16 12:12:30 +0800

    fix bug

--- src/api/news.js
+++ src/api/news.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://gnews.io/api/v4/",
   params: {
-    token: `${process.env.REACT_APP_GNEWS_API_TOKEN}`,
+    token: process.env.REACT_APP_GNEWS_API_TOKEN,
   },
 });
