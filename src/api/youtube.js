38a71182ea40b8cb203b6ac0d0161ded21cd8439
deborahlim <deborahlimhy@gmail.com>
2022-06-22 22:16:32 +0800

    remove comments

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,25 +1,3 @@
-// function youtubeSearch(term) {
-  // 2. Initialize the JavaScript client library.
-//   window.gapi.client
-//     .init({
-//       apiKey: "AIzaSyBIIjsqbgbSIv12TzL_4aKJliC17NhL3fk",
-//     })
-//     .then(function () {
-//       // 3. Initialize and make the API request.
-//       return window.gapi.client.request({
-//         path: `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${term}`,
-//       });
-//     })
-//     .then(
-//       function (response) {
-//         console.log(response.result.items);
-//         return response.result.items;
-//       },
-//       function (reason) {
-//         console.log("Error: " + reason.result.error.message);
-//       }
-//     );
-// }
 // 1. Load the JavaScript client library.
 import axios from "axios";
 
